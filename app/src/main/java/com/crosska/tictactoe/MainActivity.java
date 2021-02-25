4dfddfd909a9e371373538e4d176ff0bc1db27c4
Владислав <musical.vg@gmail.com>
2021-02-25 11:05:43 +0300

    Added actions on play button

--- app/src/main/java/com/crosska/tictactoe/MainActivity.java
+++ app/src/main/java/com/crosska/tictactoe/MainActivity.java
@@ -3,6 +3,8 @@ package com.crosska.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void play_button_clicked(View view) {
+        Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show();
     }
 }
