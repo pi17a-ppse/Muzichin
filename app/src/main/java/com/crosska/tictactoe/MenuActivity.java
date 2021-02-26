26e49b2d04cb83910b394519a9ba7dcb972e2b00
Влад Музычин <Musical.vg@gmail.com>
2021-02-26 14:39:59 +0300

    Developed functionality of menu buttons

--- app/src/main/java/com/crosska/tictactoe/MenuActivity.java
+++ app/src/main/java/com/crosska/tictactoe/MenuActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -12,5 +13,9 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    public void exit_menu_button_clicked(View view) {
+        this.finishAffinity();
     }
 }
