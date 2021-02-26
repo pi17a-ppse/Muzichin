985ca78b985c7e9e5cfdab04891b1bae458fcd4f
Влад Музычин <Musical.vg@gmail.com>
2021-02-26 16:54:26 +0300

    Added some basic elements to game activity

--- app/src/main/java/com/crosska/tictactoe/MenuActivity.java
+++ app/src/main/java/com/crosska/tictactoe/MenuActivity.java
@@ -1,10 +1,11 @@
 package com.crosska.tictactoe;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -17,5 +18,10 @@ public class MenuActivity extends AppCompatActivity {
 
     public void exit_menu_button_clicked(View view) {
         this.finishAffinity();
+    }
+
+    public void game_menu_button_clicked(View view) {
+        Intent intent = new Intent(MenuActivity.this, GameActivity.class);
+        startActivity(intent);
     }
 }
