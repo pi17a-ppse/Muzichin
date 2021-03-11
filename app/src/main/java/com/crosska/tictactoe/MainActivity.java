254404049b04404239016e8e266296a198e94ca8
Владислав <musical.vg@gmail.com>
2021-03-11 10:06:25 +0300

    temp com

--- app/src/main/java/com/crosska/tictactoe/MainActivity.java
+++ app/src/main/java/com/crosska/tictactoe/MainActivity.java
@@ -22,7 +22,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void play_button_clicked(View view) {
         Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(MainActivity.this, MenuActivity.class);
-        startActivity(intent);
+        Intent intent1 = new Intent(MainActivity.this, MenuActivity.class);
+        intent1 = new Intent(MainActivity.this, MenuActivity.class);
+        intent1 = new Intent(MainActivity.this, MenuActivity.class);
+        startActivity(intent1);
     }
 }
